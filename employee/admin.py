4d908bb87a77f01e3861bb9912b81963f0a49160
ALDS <96967226+Aladinndevolpment@users.noreply.github.com>
2024-04-23 18:45:29 +0530

    final

--- employee/admin.py
+++ employee/admin.py
@@ -1,8 +1,19 @@
+# TODO: Remove repetition. (created_at, updated_at, is_active)
 from django.contrib import admin
-from .models import Department
+from .models import Department, Designation
 # Register your models here.
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'code',
+                    'description', 'created_at', 'update_at', 'is_active']
+    search_fields = ['name']
+    list_filter = ['created_at']
+    list_editable = ['is_active']
+
+
+@admin.register(Designation)
+class DesignationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'department', 'description',
+                    'created_at', 'update_at', 'is_active']
--- employee/models.py
+++ employee/models.py
@@ -1,34 +1,35 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
 
 
-class Department(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    code = models.CharField(max_length=10, unique=True)
-    description = models.TextField(max_length=500)
+class CommonAttributes(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Department(CommonAttributes):
+    name = models.CharField(max_length=30, unique=True)
+    code = models.CharField(max_length=10, unique=True)
+    description = models.TextField(max_length=500)
 
     def __str__(self) -> str:
         return self.name
 
 
-class Designation(models.Model):
+class Designation(CommonAttributes):
     department = models.ForeignKey(Department, on_delete=models.PROTECT)
     description = models.TextField(max_length=500)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
 
 
-class Employee(models.Model):
+class Employee(CommonAttributes):
     first_name = models.CharField(max_length=30, unique=True)
     last_name = models.CharField(max_length=30, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     contact = models.CharField(max_length=15, unique=True)
     designation = models.ForeignKey(Designation, on_delete=models.PROTECT)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
